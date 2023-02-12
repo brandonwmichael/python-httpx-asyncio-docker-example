2c29178a54ad917b29a8bca39d4f79b4afe37b2b
brandonwmichael <brandon@bwmlab.com>
2023-02-11 19:37:31 -0600

    initial commit

---
+++ project/main.py
@@ -0,0 +1,40 @@
+import asyncio
+import httpx
+import datetime
+
+
+async def main():
+    # start = datetime.datetime.now()
+    
+    pokemon_url = 'https://pokeapi.co/api/v2/pokemon/151'
+
+    async with httpx.AsyncClient() as client:
+
+        resp = await client.get(pokemon_url)
+
+        pokemon = resp.json()
+        # print(pokemon['name'])
+        
+        # end = datetime.datetime.now()
+    
+        # delta = end - start
+        return pokemon['name']
+
+
+
+a = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+
+
+start = datetime.datetime.now()
+
+
+for i in a:
+    # asyncio.run(main())
+    print(datetime.datetime.now(), "Response:", asyncio.run(main()))
+
+# asyncio.run(main())
+
+end = datetime.datetime.now()
+delta = end - start
+
+print("Total Time:", delta)
